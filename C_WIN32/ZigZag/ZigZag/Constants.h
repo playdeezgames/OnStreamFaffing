3f057a494d38350c9d949429caf682b4660b68c0
ernestpazera <ernestpazera@gmail.com>
2020-06-15 05:45:13 -0500

    cleaning up and adding deploy directory

--- C_WIN32/ZigZag/ZigZag/Constants.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#pragma once
-
-
-#endif
--- C_WIN32/ZigZag/ZigZag/ZigZag.c
+++ C_WIN32/ZigZag/ZigZag/ZigZag.c
@@ -1,7 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <mmsystem.h>
-#include "Constants.h"
 #include <stdlib.h>
 
 #define CELL_COLUMNS 40
